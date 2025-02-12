9caa1c0e93922e5a51827b16b98fb408a81d8bbb
Lucas Macedo <lucasedumacedo@gmail.com>
2025-02-12 15:23:36 -0400

    hotfix: fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           {/* Botões de links para as rotas de blog */}
-          <Link className="button button--secondary button--lg" to="/sgpl">
+          <Link className="button button--secondary button--lg" to="/blog">
             Visite o Blog SGPL
           </Link>
         </div>
